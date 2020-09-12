83d3bb7377425f7930ed70c7e09d4bcf8578bb2e
陆茂斌 <me@alienx.cn>
2020-09-12 16:43:02 +0800

    更新解法

--- MEDIUM/[+++]_24_Swap Nodes in Pairs.cpp
+++ MEDIUM/[+++]_24_Swap Nodes in Pairs.cpp
@@ -17,3 +17,50 @@ public:
         return next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        vector<ListNode*> vec;
+        ListNode* ptrL = head;
+        ListNode* ptrR = head->next;
+        ListNode* start = head->next;
+
+        while (ptrL != NULL) {
+            ptrR = ptrL->next;
+            if (ptrR == NULL)
+                break;
+            ListNode* temp = ptrR->next;
+            ptrR->next = ptrL;
+            vec.push_back(temp);
+            ptrL = temp;
+        }
+
+        ListNode* ptr = head;
+        for (auto item:vec) {
+            if (item != NULL) {
+                if (item->next != NULL) {
+                    ptr->next = item->next;
+                    ptr = ptr->next->next;
+                } else {
+                    ptr->next = item;
+                }
+            } else {
+                ptr->next = NULL;
+            }
+        }
+        return start;
+    }
+};
